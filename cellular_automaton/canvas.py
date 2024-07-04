50959198a2bfc84c251ac8da56f2f2a523d74665
flo <flo@flo.flo>
2024-07-04 18:58:28 +0200

    worked on canvas logic

--- cellular_automaton/canvas.py
+++ cellular_automaton/canvas.py
@@ -4,14 +4,22 @@ class Canvas:
     def __init__(self, width=500, height=500):
         self.width = width
         self.height = height
-        self.canvas : np.ndarray = np.zeros((height, width, 3), dtype=np.uint8)
+
+        self.canvas_states : np.ndarray = np.zeros((width, height), dtype=np.uint8)
+        self.canvas_change_flags : np.ndarray = np.zeros((width, height), dtype=np.uint8)
+        self.canvas_rgb : np.ndarray = np.zeros((width, height, 3), dtype=np.uint8)
 
     def step(self):
-        for (x,y), value in np.ndenumerate(self.canvas):
+        for (x,y), value in np.ndenumerate(self.canvas_rgb):
             pass
 
     def draw(self, x, y, color):
-        self.canvas[y, x] = color
+        radius = 10
+        x, y = x-1, y-1
+        for _x in range(max(0, x-radius), min(self.width-1, x+radius)):
+            for _y in range(max(0, y-radius), max(self.height-1, y+radius)):
+                if (_x - x)**2 + (_y - y)** 2 < radius**2:
+                    self.canvas_rgb[_x, _y] = color
 
     def get_canvas(self):
-        return self.canvas
+        return self.canvas_rgb
--- cellular_automaton/main.py
+++ cellular_automaton/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, WebSocket
+import asyncio
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from .canvas import Canvas
@@ -7,19 +8,39 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-canvas = Canvas(1500,600)
+canvas = Canvas(1200,500)
+connected_clients = []
 
 @app.get("/")
 async def get():
-    return HTMLResponse(open("static/index.html").read())
+    """ defines get request from root path """
+    return HTMLResponse(open("static/index.html", "r", encoding="utf-8").read())
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """ awaits and handles responses from websocket """
     await websocket.accept()
+    connected_clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            if data["action"] == "draw":
+                x, y, color = data["x"], data["y"], data["color"]
+                canvas.draw(x, y, color)
+            elif data["action"] == "get_canvas":
+                await websocket.send_json({"canvas": canvas.get_canvas().tolist()})
+    except WebSocketDisconnect:
+        connected_clients.remove(websocket)
+
+async def broadcast_canvas_state():
+    """ handles broadcasting the same canvas to every client """
     while True:
-        data = await websocket.receive_json()
-        if data["action"] == "draw":
-            x, y, color = data["x"], data["y"], data["color"]
-            canvas.draw(x, y, color)
-        elif data["action"] == "get_canvas":
-            await websocket.send_json({"canvas": canvas.get_canvas().tolist()})
+        canvas_state = {"canvas": canvas.get_canvas().tolist()}
+        for client in connected_clients:
+            await client.send_json(canvas_state)
+        await asyncio.sleep(.1)
+
+@app.on_event("startup")
+async def startup_event():
+    """ creates async task on startup """
+    asyncio.create_task(broadcast_canvas_state())
